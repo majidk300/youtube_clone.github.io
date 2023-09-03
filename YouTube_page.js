6ba00090701027d476dc26766853a1a83436134a
Majid Naseem <majidnaseem726@gmail.com>
2023-09-03 17:40:12 +0530

    youtubec_clone

---
+++ YouTube_page.js
@@ -0,0 +1,11 @@
+const openNav = () =>{
+				document.getElementById("sidenavboxes").style.left="0";
+}
+
+
+const closeNav = () =>{
+	
+	document.getElementById("sidenavboxes").style.left="-90%";
+
+}
+  
